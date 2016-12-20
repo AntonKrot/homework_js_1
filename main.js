7ddd01ed41b2b6cb0be79368c3264d375b80447c
antonKrot <anton.kopchak@gmail.com>
2016-12-20 23:46:13 +0200

    homework js 20.12.2016

---
+++ main.js
@@ -0,0 +1,161 @@
+/**
+ * Created by admin on 16.12.2016.
+ */
+
+
+/*--------------- Поаток питаннь ------------------------*/
+    var a = +prompt ("Скільки буде 5 + 5");
+    if (a == 10) {
+    } else if (a != 10) {
+    } else {
+    }
+
+    var b = +prompt ("Скільки буде 2 + 2");
+    if (b == 4) {
+    } else if (b != 4) {
+    } else {
+    }
+
+    var c = +prompt ("Скільки буде 7 + 5");
+    if (c == 12) {
+    } else if (c != 12) {
+    } else {
+    }
+
+    var d = +prompt ("Скільки буде 8 X 5");
+    if (d == 40) {
+    } else if (d != 40) {
+    } else {
+    }
+
+    var e = +prompt ("Скільки буде 6 X 5");
+    if (e == 30) {
+    } else if (e != 30) {
+    } else {
+    }
+
+    var f = +prompt ("Скільки буде 11 + 11 ");
+    if (f == 22) {
+    } else if (f != 22) {
+    } else {
+    }
+
+    var g = +prompt ("Скільки буде 110 X 10");
+    if (g == 1100) {
+    } else if (g != 1100) {
+    } else {
+    }
+
+    var h = +prompt ("Скільки буде 8 X 5");
+    if (h == 40) {
+    } else if (h != 40) {
+    } else {
+    }
+
+    var i = +prompt ("Скільки буде 3 + 5");
+    if (i == 8) {
+    } else if (i != 8) {
+    } else {
+    }
+
+    var j = +prompt ("Скільки буде 40 X 2");
+    if (j == 80) {
+    } else if (j != 80) {
+    } else {
+    }
+/*--------------- Кінець питаннь ------------------------*/
+    var sum1 = 0;
+
+    if (a == 10) {
+        var sum2 = ++sum1;
+    } else if (a != 10) {
+        var sum2 = sum1;
+    } else {
+    }
+
+    var sum2;
+
+    if (b == 4) {
+        var sum3 = ++sum2;
+    } else if (b != 4) {
+        var sum3 = sum2;
+    } else {
+    }
+
+    var sum3;
+
+    if (c == 12) {
+        var sum4 = ++sum3;
+    } else if (c != 12) {
+        var sum4 = sum3;
+    } else {
+    }
+
+    var sum4;
+
+    if (d == 40) {
+        var sum5 = ++sum4;
+    } else if (d != 40) {
+        var sum5 = sum4;
+    } else {
+    }
+
+    var sum5;
+
+    if (e == 30) {
+        var sum6 = ++sum5;
+    } else if (e != 30) {
+        var sum6 = sum5;
+    } else {
+    }
+
+    var sum6;
+
+    if (f == 22) {
+        var sum7 = ++sum6;
+    } else if (f != 22) {
+        var sum7 = sum6;
+    } else {
+    }
+
+    var sum7;
+
+    if (g == 1100) {
+        var sum8 = ++sum7;
+    } else if (g != 1100) {
+        var sum8 = sum7;
+    } else {
+    }
+
+    var sum8;
+
+    if (h == 40) {
+        var sum9 = ++sum8;
+    } else if (g != 1100) {
+        var sum9 = sum8;
+    } else {
+    }
+
+    var sum9;
+
+    if (i == 8) {
+        var sum10 = ++sum9;
+    } else if (i != 8) {
+        var sum10 = sum9;
+    } else {
+    }
+
+    var sum10;
+
+    if (j == 80) {
+        var sum11 = ++sum10;
+    } else if (j != 80) {
+        var sum11 = sum10;
+    } else {
+    }
+
+    alert("Правильних відповідей: "  +sum11);
+
+
+
+
